fa6a7623d1e1150f044c2c17f859b45b4ff6a422
AdamT-S <158591813+AdamT-S@users.noreply.github.com>
2024-06-20 20:55:50 +0100

    fixed errors

--- src/models/city.mjs
+++ src/models/city.mjs
@@ -1,16 +1,21 @@
 export default class City {
 	id;
 	name;
+	region;
 	countryCode;
 	district;
-	population;
 	country;
+	continent;
+	population;
 
-	constructor(id, name, countryCode, district, population) {
+	constructor(id, name, region, countryCode, district, population, country, continent) {
 		this.id = id;
 		this.name = name;
+		this.region = region;
 		this.countryCode = countryCode;
 		this.district = district;
 		this.population = population;
+		this.country = country;
+		this.continent = continent;
 	}
 }
